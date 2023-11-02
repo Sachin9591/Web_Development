5d25a30250c1178f9b314816ad29ff6beb54e1db
SACHIN KUMAR B <skumar10031993@gmail.com>
2023-11-02 15:37:02 +0530

    Update script.js

--- Basic Calculator/script.js
+++ Basic Calculator/script.js
@@ -1,67 +1,80 @@
-let firstNumber = "";
-let secondNumber = "";
-let operator = "";
-let result = "";
-let resetScreen = false;
-let numButtons = document.getElementsByClassName("numButton");
-let opButtons = document.getElementsByClassName("opButton");
-let eqButton = document.getElementsByClassName("eqButton")[0];
-let clearButton = document.getElementsByClassName("clearButton")[0];
-let displays = document.getElementById('display');
+/* Define global variables for storing operand, operator values */
+        let firstOperand = "";
+        let secondOperand = "";
+        let currentOperator = null;
+        let resetScreen = false;
 
+        /* Get reference to the display and buttons on the page */
+        const display = document.querySelector("#display");
+        const numButtons = document.querySelectorAll(".numButton");
+        const opButtons = document.querySelectorAll(".opButton");
+        const eqButton = document.querySelector(".eqButton");
+        const clearButton = document.querySelector(".clearButton");
 
-function updateDisplay(x){
-    displays.textContent = x;
-}
+        /* Event listener for number buttons. On click, append the number to the display */
+        numButtons.forEach(button => {
+            button.addEventListener('click', () => appendNumber(button.textContent));
+        });
 
+        /* Event listener for operator buttons. On click, store the first operand and set the current operator */
+        opButtons.forEach(button => {
+            button.addEventListener('click', () => setOperation(button.textContent));
+        });
 
-for(let i=0; i<numButtons.length; i++){
-    numButtons[i].addEventListener("click", function(){
-        if(operator == ""){
-            firstNumber += this.textContent;
-            updateDisplay(firstNumber);
-        }else{
-            secondNumber += this.textContent;
-            updateDisplay(secondNumber);
-        }
-    });
-}
+        /* Event listener for equals button. On click, perform the calculation and display the result */
+        eqButton.addEventListener('click', () => compute());
 
-for (let i = 0; i < opButtons.length; i++) {
-    opButtons[i].addEventListener("click", function() {
-        operator = this.textContent;
-    });
-}
+        /* Event listener for clear button. On click, clear all stored data and reset the display */
+        clearButton.addEventListener('click', () => clear());
 
-eqButton.addEventListener("click", function() {
-    result = operate(operator, parseFloat(firstNumber), parseFloat(secondNumber));
-    updateDisplay(result);
-});
-
-clearButton.addEventListener("click", function() {
-    firstNumber = "";
-    secondNumber = "";
-    operator = "";
-    result = "";
-    updateDisplay(0);
-});
-
-
-function operate(operator, a, b) {
-    switch(operator) {
-        case '+':
-            return a + b;
-        case '-':
-            return a - b;
-        case '*':
-            return a * b;;
-        case '/':
-            if(b == 0) {
-                return 'Error! Division by zero is undefined';
-            } else {
-                return a / b;
+        /* Function to append the clicked number to the display */
+        function appendNumber(num) {
+            if (resetScreen) {
+                display.textContent = "";
+                resetScreen = false;
             }
-        default:
-            return null;
-    }
-}
+            display.textContent += num;
+        }
+
+        /* Function to set the operator and store the first operand */
+        function setOperation(op) {
+            if (currentOperator !== null) compute();
+            firstOperand = display.textContent;
+            currentOperator = op;
+            resetScreen = true;
+        }
+
+        /* Function to perform the calculation and display the result */
+        function compute() {
+            secondOperand = display.textContent;
+            display.textContent = operate(currentOperator, firstOperand, secondOperand);
+            currentOperator = null;
+        }
+
+        /* Function to clear all stored data and reset the display */
+        function clear() {
+            display.textContent = "0";
+            firstOperand = "";
+            secondOperand = "";
+            currentOperator = null;
+        }
+
+        /* Function to perform the selected operation on the two operands */
+        function operate(operator, a, b) {
+            a = Number(a);
+            b = Number(b);
+            switch (operator) {
+                case '+':
+                    return a + b;
+                case '-':
+                    return a - b;
+                case '*':
+                    return a * b;
+                case '/':
+                    /* Error handling for division by zero */
+                    if (b === 0) return 'Error! Div by 0';
+                    return a / b;
+                default:
+                    return b;
+            }
+        }
